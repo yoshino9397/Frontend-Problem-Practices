51072fda227b7d25def54fa6c18bd40239a83aa9
yoshino9397 <yosino8512@gmail.com>
2022-08-21 16:27:59 -0700

    workatTech challenge NOW

--- WorkTech-BasicCoding/script.js
+++ WorkTech-BasicCoding/script.js
@@ -43,3 +43,111 @@ let solve6 = (r, h) => {
 };
 solve6(5, 24);
 
+///Average Weight
+let solve7 = (...input) => {
+  let array = [...input];
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
+};
+console.log(
+  solve7(40.75, 45.2, 55.3, 49.5, 43.3, 54.1, 38.4, 63.8, 45.2, 58.25)
+);
+
+///Odd Even
+let solve8 = (...input) => {
+  let array = [...input];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      console.log("EVEN");
+    } else {
+      console.log("ODD");
+    }
+  }
+};
+solve8(23, 12, 44, 32);
+
+///Print Digits
+let solve9 = (...input) => {
+  let array = [...input];
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i].toString().split("").join(" "));
+  }
+};
+solve9(34, 45);
+
+///Divisible By Six
+let solve10 = (...input) => {
+  let array = [...input];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 6 === 0) {
+      console.log("TRUE");
+    } else {
+      console.log("FALSE");
+    }
+  }
+};
+solve10(457, 66, 0, 40);
+
+///Temperature Conversion
+let solve11 = (input) => {
+  return (9 * input) / 5 + 32;
+};
+console.log(solve11(12));
+
+///7 Up 7 Down
+let solve12 = (...input) => {
+  let array = [...input];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 7) {
+      console.log("UP");
+    } else if (array[i] === 7) {
+      console.log("EQUAL");
+    } else {
+      console.log("DOWN");
+    }
+  }
+};
+solve12(12, 7, 4);
+
+///Triangle
+let solve13 = (...input) => {
+  let array = [...input];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][0] + array[i][1] > array[i][2]) {
+      console.log(array[i][0] + array[i][1] + array[i][2]);
+    } else {
+      console.log(array[i][0] + array[i][1] - array[i][2]);
+    }
+  }
+};
+solve13([3, 4, 5], [1, 2, 4]);
+
+///Number Reversal
+let solve14 = (input) => {
+  let arr = input.toString().split("").reverse().join("");
+  return console.log(Number(arr));
+};
+solve14(237);
+
+///Add One
+let solve15 = (...input) => {
+  let info = [...input];
+  console.log(
+    `Hello ${info[0]}! Next year, you will be ${info[1] + 1} years old`
+  );
+};
+solve15("Ram", 15);
+
+///Happy Birthday
+let solve16 = (...input) => {
+  let info = [...input];
+  let date = new Date();
+  let year = date.getFullYear();
+  console.log(
+    `Happy Birthday ${info[0]}! Your current age is ${year - info[1]}`
+  );
+};
+solve16("Ram", 1996);
